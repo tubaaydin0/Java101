663d44f576cce91f8cb5dd846762503da8668270
tubaaydin0 <tuba.aydin94@gmail.com>
2023-08-29 01:15:04 +0300

    dizinin transpozunu bulma

---
+++ diziTranspoze/diziTranspoz/src/Main.java
@@ -0,0 +1,35 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int[][] dizi={
+                {1,2,3},
+                {4,5,6},
+        };
+        int [][] tDizi=new int[3][2];
+
+        System.out.println("Matris:");
+
+        for (int i=0; i<dizi.length; i++){
+            for (int j=0; j< dizi[i].length; j++){
+                System.out.print(dizi[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        for (int i=0; i<tDizi.length; i++){
+            for (int j=0; j< tDizi[i].length; j++){
+                tDizi[i][j]=dizi[j][i];
+            }
+        }
+        System.out.println("Transpoze:");
+
+        for (int i=0; i< tDizi.length;i++){
+            for (int j=0; j<tDizi[i].length;j++){
+                System.out.print(tDizi[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
