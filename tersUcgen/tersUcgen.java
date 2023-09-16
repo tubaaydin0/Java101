e3dac5cc3cfeae9a89bc636b53da2fbd51400dda
tubaaydin0 <tuba.aydin94@gmail.com>
2023-09-16 16:27:57 +0300

    tersucgen

--- tersUcgen/tersUcgen.java
+++ tersUcgen/tersUcgen.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 
 public class tersUcgen {
 	 public static void main(String[] args) {
-		 /*			boþluk		yýldýz		satýr:5
+		 /*			boÅŸluk		yÄ±ldÄ±z		satÄ±r:5
 		  *****			0		5
 		   ***			2		3
 		    *			4		1
-		  
 		  */
 		 int satir;
 		 Scanner inp = new Scanner(System.in);
-		 System.out.print("Satýr sayýsý giriniz: ");
+		 System.out.print("SatÄ±r sayÄ±sÄ±nÄ± giriniz: ");
 		 satir=inp.nextInt();
 		 
 		 for(int i=satir; i>=0 ;i--) {
