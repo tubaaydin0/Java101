852ba78780ea3dddeed2b12a8be02b02c4ac4f3e
tubaaydin0 <tuba.aydin94@gmail.com>
2023-09-16 14:55:09 +0300

    Artık yıl hesaplama
    /*Java ile kullanıcının girdiği yılın artık olup olmadığını bulan programı yazınız.
    
     Artık Yıl Nedir?
    
     Artık yıl, Miladî takvimde 365 yerine 366 günü olan yıl. Bu fazladan gün (artık gün),
     normalde 28 gün olan şubat ayına 29 Şubat’ın eklenmesi ile elde edilir.
    
      Artık Yıl Nasıl Hesaplanır?
    
      Genel bir kural olarak, artık yıllar 4 rakamının katı olan yıllardır:
    
      1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024 gibi.
      100'ün katı olan yıllardan sadece 400'e kalansız olarak bölünebilenler artık yıldır:
    
         Örneğin 1200, 1600, 2000 yılları artık yıldır ancak 1700, 1800 ve 1900 artık yıl değildir.*/

--- artikYilHesaplama/src/Main.java
+++ artikYilHesaplama/src/Main.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
+/*Java ile kullanıcının girdiği yılın artık olup olmadığını bulan programı yazınız.
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+ Artık Yıl Nedir?
+
+ Artık yıl, Miladî takvimde 365 yerine 366 günü olan yıl. Bu fazladan gün (artık gün),
+ normalde 28 gün olan şubat ayına 29 Şubat’ın eklenmesi ile elde edilir.
+
+  Artık Yıl Nasıl Hesaplanır?
+
+  Genel bir kural olarak, artık yıllar 4 rakamının katı olan yıllardır:
+
+  1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024 gibi.
+  100'ün katı olan yıllardan sadece 400'e kalansız olarak bölünebilenler artık yıldır:
+
+     Örneğin 1200, 1600, 2000 yılları artık yıldır ancak 1700, 1800 ve 1900 artık yıl değildir.*/
+
 public class Main {
     public static void main(String[] args) {
         int yuzlerBas,binlerBas,sayi,sonIki;
